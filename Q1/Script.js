6a3a7edfa76270ac86d119bb50e646b639533af2
Vineeksha V <108666166+Vineekshac@users.noreply.github.com>
2023-03-18 13:19:12 +0530

    Add files via upload

---
+++ Q1/Script.js
@@ -0,0 +1,54 @@
+let myResume={
+    "basics": {
+      "name": "Vineeksha V",
+      "email": "vineekshac@gamil.com",
+      "phone": 9353234711,
+      "degree": "MSc.",
+      "location": {
+        "address": "Daregudde",
+        "postalCode": 574213,
+        "city": "Mangalore",
+        "state": "Karanataka",
+        "country": "India"
+      },
+    },
+    "work": [
+      {
+        "company": "Infosys Ltd",
+        "position": "System Engineer",
+        "startDate": "2021-09-30",
+        "summary": "i am the Application developer handling various task ",
+      },
+    ],
+    "education": [
+      {
+        "institution": "Alva's College,Moodabidri",
+        "department": "Computer Science",
+        "studyType": "fulltime",
+        "batch start year": 2017,
+        "batch end year": 2019,
+        "gpa": 8.6,
+      }
+    ],
+    "skills": [
+      {
+        "name": "python,Java,javascript,sql",
+        "level": "beginer",
+        "project": [
+          "Image processing door lock system using Matlab"
+        ]
+      }
+    ],
+    "languages": [
+      {
+        "language": "English,Kannada,Hindi,Tulu",
+      }
+    ],
+    "interests": [
+      {
+        "name": "Internet scurfing,Cooking,Drawing",
+      }
+    ]
+  }
+  console.log(myResume);
+  
